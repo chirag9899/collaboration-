98ddf22acf9cc5bdd2bfd25baaef5666dd8f301c
fullstackdeveloper710@gmail.com <fullstackdeveloper710@gmail.com>
2024-04-18 16:21:55 +0530

    fixed menu dropdown design

--- components/header.js
+++ components/header.js
@@ -3,99 +3,22 @@ import { useRef } from "react";
 import Link from "next/link";
 import { useOnClickOutside } from "frontedUtils/hooks";
 import Account from "./account";
-import {
-  p_12_normal,
-  p_16_semibold,
-  p_18_semibold,
-} from "../styles/textStyles";
+import { p_16_semibold } from "./styles/textStyles";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setShowHeaderMenu,
   showHeaderMenuSelector,
 } from "../store/reducers/showConnectSlice";
-import { ReactComponent as Plus } from "../public/imgs/icons/plus.svg";
-import { ReactComponent as Discussions } from "../public/imgs/icons/discussions.svg";
 import { Flex } from "@osn/common-ui";
 import Menu from "@/components/menu";
 import NotificationBell from "./notification/bell";
-import { ReactComponent as CaretRight } from "/public/imgs/icons/caret-right-s.svg";
 import React from "react";
 import { MOBILE_SIZE } from "@osn/constants";
-import LogoImg from "../public/imgs/beravote-logo.svg";
-import LogoIcon from "../public/imgs/beravote-logoIcon.svg";
-import { dark_violet, primary_text_color, text_light_major } from "./styles/colors";
+import LogoImg from "../public/imgs/dvote.logo.svg";
+import LogoIcon from "../public/imgs/logoIcon.svg";
+import { primary_text_color, text_light_major } from "./styles/colors";
 import Image from "next/image";
-
-const CaretRightIcon = styled(CaretRight)`
-  margin-left: 16px;
-`;
-
-const AppWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  ${p_18_semibold};
-  height: 36px;
-  color: ${text_light_major};
-  cursor: pointer;
-
-  &:hover {
-    color: ${primary_text_color};
-    .hoverMenu {
-      display: flex;
-      flex-wrap: wrap;
-    }
-    .onHoverReverse {
-      transform: rotate(180deg);
-    }
-  }
-
-  > img {
-    width: 24px;
-    margin-right: 8px;
-  }
-
-  span {
-    margin-right: 4px;
-  }
-`;
-
-const HoverMenu = styled.div`
-  padding: 16px;
-  position: absolute;
-  display: none;
-  gap: 24px;
-  background-color: ${dark_violet};
-  &:hover {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  z-index: 1;
-  top: 60px;
-  width: 360px;
-  box-shadow: 0 4px 31px rgba(26, 33, 44, 0.06),
-    0px 0.751293px 8px rgba(26, 33, 44, 0.04);
-`;
-
-const MenuItem = styled.a`
-  display: flex;
-  align-items: start;
-  justify-content: center;
-  flex-wrap: wrap;
-  p,
-  span {
-    margin: 0;
-    width: 232px;
-  }
-  p {
-    margin-left: 16px;
-    font-size: 16px;
-  }
-  span {
-    ${p_12_normal};
-    color: ${text_light_major};
-  }
-`;
 
 const HeaderItemWrapper = styled.div`
   display: flex;
@@ -112,10 +35,28 @@ const HeaderItemWrapper = styled.div`
     left: 0;
     right: 0;
   }
+  @media screen and (max-width: 767px) {
+    display: none !important;
+  }
 `;
 
-const SecondaryHeaderItemWrapper = styled(HeaderItemWrapper)`
-  background-color: var(--neutral-1);;
+const SecondaryHeaderItemWrapper = styled.div`
+  background-color: var(--background);
+  display: flex;
+  gap: 32px;
+  transition: all 0.5s;
+
+  @media screen and (max-width: 800px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    z-index: 1;
+    position: absolute;
+    top: 62px;
+    left: 0;
+    right: 0;
+  }
   @media screen and (min-width: 800px) {
     display: none;
   }
@@ -131,9 +72,9 @@ const IconWrapper = styled.div`
 
 const ExternalLinkWrapper = styled(Flex)`
   gap: 32px;
-  @media screen and (max-width: 800px) {
+  /* @media screen and (max-width: 800px) {
     display: none;
-  }
+  } */
 `;
 
 const ExternalLink = styled.a`
@@ -188,15 +129,7 @@ const Logo = styled.div`
   }
 `;
 
-const Divider = styled.div`
-  width: 1px;
-  height: 16px;
-  margin: 0 16px;
-  background: var(--background);
-`;
-
 const ChildWrapper = styled(Flex)`
-  flex: 1;
   justify-content: space-between;
 `;
 
@@ -213,7 +146,6 @@ export default function Header({ networks }) {
 
   const ref = useRef();
   useOnClickOutside(ref, (event) => {
-    // connect modal is at body level, doesn't contained in the <Header/>, so exclude manually
     if (document?.querySelector(".modals")?.contains(event.target)) {
       return;
     }
@@ -229,34 +161,8 @@ export default function Header({ networks }) {
           <Link href="/">
             <Logo />
           </Link>
-          <Divider />
         </Flex>
         <ChildWrapper>
-          <AppWrapper>
-            {/* <span>Voting</span>
-            <img
-              className="onHoverReverse"
-              src="/imgs/icons/caret-down-s.svg"
-              alt=""
-            />
-            <HoverMenu className="hoverMenu">
-              <MenuItem href="/">
-                <img src="/imgs/icons/voting.svg" alt="" />
-                <p>Off-chain Voting</p>
-                <CaretRightIcon />
-                <span>
-                  Multi-chain assets off-chain voting platform for Polkadot
-                  ecosystem
-                </span>
-              </MenuItem>
-              <MenuItem href="https://dvote.ai/">
-                <img src="/imgs/icons/short-term-employment.svg" alt="" />
-                <p>Bounties</p>
-                <CaretRightIcon />
-                <span>Decentralized bounty collaboration platform</span>
-              </MenuItem>
-            </HoverMenu> */}
-          </AppWrapper>
           <IconWrapper
             onClick={() => {
               dispatch(setShowHeaderMenu(!showMenu));
@@ -274,15 +180,10 @@ export default function Header({ networks }) {
               <ExternalLinkWrapper>
                 <Link href="/space/new" passHref legacyBehavior>
                   <InternalLink>
-                  <i className="icon-plus"></i>
+                    <i className="icon-plus"></i>
                     Add a Space
                   </InternalLink>
                 </Link>
-                {/* <ExternalLink
-                  target="_blank"
-                  href="https://dvote.ai/"
-                ><i class="icon-message-circle"></i> Discussions
-                </ExternalLink> */}
               </ExternalLinkWrapper>
             )}
             <AccountAndBell>
@@ -292,7 +193,7 @@ export default function Header({ networks }) {
           </HeaderItemWrapper>
           {showMenu && (
             <SecondaryHeaderItemWrapper>
-              <Menu />
+              <Menu networks={networks} />
             </SecondaryHeaderItemWrapper>
           )}
         </ChildWrapper>
