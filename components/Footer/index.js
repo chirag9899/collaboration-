f16efb87358b1dfee31cd64346b0c5e5e0504035
fullstackdeveloper710@gmail.com <fullstackdeveloper710@gmail.com>
2024-04-19 11:48:14 +0530

    Changed logos

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import ExternalLink from "../ExternalLink";
-import { ReactComponent as FooterLogoSvg } from "../../public/imgs/beravote-logo.svg";
+import { ReactComponent as FooterLogoSvg } from "../../public/imgs/beravote-logoIcon.svg";
 import { ReactComponent as GithubSvg } from "../../public/imgs/icons/github.svg";
 import { ReactComponent as DiscordSvg } from "../../public/imgs/icons/discord.svg";
 import { ReactComponent as TwitterSvg } from "../../public/imgs/icons/twitter.svg";
@@ -97,8 +97,8 @@ const RightWrapper = styled.div`
   align-items: flex-end;
 
   svg {
-    width: 140px !important;
-    height: 28px !important;
+    width: 68px !important;
+    height: 48px !important;
   }
 
   > div {
