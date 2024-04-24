fc1c8bd35df3b04af29e7e6e519efcb72915978f
fullstackdeveloper710@gmail.com <fullstackdeveloper710@gmail.com>
2024-04-24 16:17:40 +0530

    fixed font design issue

--- components/newSpace/sider/styled.js
+++ components/newSpace/sider/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Name = styled.span`
   font-style: normal;
-  font-weight: 500;
+  font-weight: bold;
   font-size: 14px;
   line-height: 24px;
   color: var(--neutral-4);
