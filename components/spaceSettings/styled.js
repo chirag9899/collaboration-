66b465ffd2ebdf3f07fadeb5cd1c2f5924fe98cc
fullstackdeveloper710@gmail.com <fullstackdeveloper710@gmail.com>
2024-06-04 19:00:47 +0530

    working on hide and show space features

--- components/spaceSettings/styled.js
+++ components/spaceSettings/styled.js
@@ -1,15 +1,19 @@
 import { styled } from "styled-components";
-import Panel from "../styled/panel";
+import panel from "../accordionPanel/panel";
 
 export const Wrapper = styled.div`
   max-width: 100%;
-  display: flex;
   justify-content: center;
+  >div{
+    margin-bottom: 50px !important;
+    max-width: 800px;
+    margin: auto;
+  }
 `;
 
-export const PanelWrapper = styled(Panel)`
+export const PanelWrapper = styled(panel)`
   min-height: 400px;
-  width: 700px;
+  max-width: 700px !important; 
   @media screen and (max-width: 800px) {
     margin: auto;
   }
@@ -18,7 +22,25 @@ export const PanelWrapper = styled(Panel)`
 export const BtnsGroup = styled.div`
   margin-top: 20px;
   text-align: center;
-  >button{
+  > button {
     margin-left: 10px;
   }
 `;
+
+export const Heading = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-bottom: 20px;
+`;
+
+export const DropdownGroup = styled.div`
+  display: flex;
+`;
+export const DropWrapper = styled.div``;
