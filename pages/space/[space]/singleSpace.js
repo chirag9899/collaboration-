83216576c96ca7f3fee7848b97cd87020a4ead06
fullstackdeveloper710@gmail.com <fullstackdeveloper710@gmail.com>
2024-07-04 16:59:14 +0530

    added Bgt balance

--- pages/space/[space]/singleSpace.js
+++ pages/space/[space]/singleSpace.js
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ssrNextApi } from "services/nextApi";
 import { to404 } from "../../../frontedUtils/serverSideUtil";
-import { useDispatch } from "react-redux";
-import { setAvailableNetworks } from "store/reducers/accountSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addressSelector,
+  setAvailableNetworks,
+} from "store/reducers/accountSlice";
 import { initAccount } from "store/reducers/accountSlice";
 import { useRouter } from "next/router";
 import { getBerachainProposals } from "helpers/beraProposals";
@@ -12,6 +15,10 @@ import Seo from "@/components/seo";
 import pick from "lodash.pick";
 import dynamic from "next/dynamic";
 import SpacePostTable from "@/components/spacePostTable";
+import useEthApis from "hooks/useEthApis";
+import { _handleChainSelect } from "@/components/connect/helper";
+import { chainMap } from "frontedUtils/consts/chains";
+import { newErrorToast } from "store/reducers/toastSlice";
 
 const BeraListInfo = dynamic(() => import("components/beraListInfo"), {
   ssr: false,
@@ -52,7 +59,59 @@ const HeaderWrapper = styled.div`
 export default function List({ spaceId, space, allProposalList }) {
   const dispatch = useDispatch();
   const [showContent, setShowContent] = useState("proposals-all");
+  const [balance, setBalance] = useState("0.00");
+  const address = useSelector(addressSelector);
   const router = useRouter();
+  const { getBalance } = useEthApis();
+
+  const handleChainSelect = async (chain) => {
+    try {
+      await _handleChainSelect(
+        connectedWallet,
+        dispatch,
+        address,
+        chainMap,
+        chain,
+      );
+      const currentChainId = await window.ethereum.request({
+        method: "eth_chainId",
+      });
+      const bartioNetworkId = chainMap.get(chain.network).id;
+
+      if (currentChainId !== bartioNetworkId) {
+        return false;
+      }
+      return true;
+    } catch (error) {
+      console.error("Error switching network:", error);
+      dispatch(newErrorToast(error.message));
+      return false;
+    }
+  };
+
+  const fetchBalance = useCallback(async () => {
+    try {
+      const bartioNetwork = { network: "berachain-b2" };
+      const switched = await handleChainSelect(bartioNetwork);
+      if (switched) {
+        const data = await getBalance(
+          address,
+          "0xbDa130737BDd9618301681329bF2e46A016ff9Ad",
+        );
+        if (data.result) {
+          setBalance(data.result);
+        } else {
+          setBalance("0.00");
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+    }
+  }, [address, getBalance]);
+
+  useEffect(() => {
+    fetchBalance();
+  }, []);
 
   useEffect(() => {
     dispatch(initAccount());
@@ -92,7 +151,11 @@ export default function List({ spaceId, space, allProposalList }) {
           {showContent.match(/proposals/) && (
             <MainWrapper>
               <HeaderWrapper>
-                <BeraListInfo spaceId={spaceId} space={space} />
+                <BeraListInfo
+                  spaceId={spaceId}
+                  space={space}
+                  balance={balance}
+                />
               </HeaderWrapper>
               <SpacePostTable
                 posts={allProposalList.proposalsWithSupports}
