bdd466594fe1f05e07a1f93f3932fdcd902fd3e9
fullstackdeveloper710@gmail.com <fullstackdeveloper710@gmail.com>
2024-07-22 18:21:19 +0530

    image issue fixed

--- components/newSpace/sider/logo.js
+++ components/newSpace/sider/logo.js
@@ -16,7 +16,7 @@ export default function Logo({ imageFile }) {
   return (
     <Wrapper>
       {imageFile && (
-        <Image
+        <img
           style={{ objectFit: "cover" }}
           width="100%"
           height="100%"
